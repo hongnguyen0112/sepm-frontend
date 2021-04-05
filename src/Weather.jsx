f4160d5f5acf3a5436e10cf26c0a19b986055374
Hong Nguyen <s3757107@rmit.edu.vn>
2021-04-05 11:16:46 +0700

    my Project

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,9 +1,6 @@
 //Import libraries
 import React, {useState} from 'react'
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng
-} from "react-places-autocomplete";
+import PlacesAutocomplete, {geocodeByAddress,getLatLng} from "react-places-autocomplete";
 
 //API set up
 const api = {
@@ -21,6 +18,7 @@ function Weather() {
         lat: null,
         lng: null
     });
+    const [address, setAddress] = useState("");
 
     //Search event (fetching the weather data from API)
     const search = evt =>{
@@ -30,68 +28,57 @@ function Weather() {
         fetch(url)
         .then(res=> res.json()) //Format the result to JSON
         .then(json => {
-          setWeather(json); //Set data of JSON file to weather
-          setLocation(""); //Set location to null
-          setCoordinates({ //Set coordinates value to null after searching
-              lat: null,
+            setWeather(json); //Set data of JSON file to weather
+            setLocation(""); //Set location to null
+            setCoordinates({
+              lat:null,
               lng: null
-          });
-          
+            })
         });
         //Test the URL
-        console.log(url)
+        console.log(url) 
     }
+    
 
     //async/await function to get the data from Google Maps API
     const handleSelect = async value => {
         const results = await geocodeByAddress(value); //Get geocode by address
         const latLng = await getLatLng(results[0]); //Get latitude and longtitude values
         setLocation(value); //Set the location to the value
+        setAddress(value);
         setCoordinates(latLng); //Set latitude and longtitude
     };
 
-    // const dateBuilder = (d) =>{
-    //   let months = ["Jan", "Feb", "Mar", "Arpil", "May", "June", "July", "August", "Sep", "Oct", "Nov", "Dec"];
-    //   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    //   let day = days[d.getDay()];
-    //   let date = d.getDate();
-    //   let month = months[d.getMonth()];
-    //   let year = d.getFullYear();
-  
-    //   return `${day} ${date} ${month} ${year}`
-    // }
+    
     return (
         <div>
             <PlacesAutocomplete
                 value={location}
                 onChange={setLocation}
                 onSelect={handleSelect}>
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
-            <input {...getInputProps({ placeholder: "Search for location..." })} /> 
-            <div>
-              {loading ? <div>...loading</div> : null} 
-              {suggestions.map(suggestion => {
-                const style = {
-                  backgroundColor: suggestion.active ? "#41b6e6" : "#fff"
-                };
-                return (
-                  <div {...getSuggestionItemProps(suggestion, { style })}>
-                    {suggestion.description};
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
-      </PlacesAutocomplete>
-      <button onClick = {search}>Search</button>
-      {(typeof weather.lat != 'undefined' && typeof weather.lon!='undefined')?(
+                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                <div>
+                    <input {...getInputProps({ placeholder: "Search for location..." })}/> 
+                    <div>
+                        {loading ? <div>...loading</div> : null} 
+                        {suggestions.map(suggestion => {
+                        const style = { backgroundColor: suggestion.active ? "#41b6e6" : "#fff" };
+                        return (
+                            <div {...getSuggestionItemProps(suggestion, { style })}>
+                            {suggestion.description}
+                        </div>);
+                    })}
+                </div>
+            </div>)}
+            </PlacesAutocomplete>
+            
+            <button onClick = {search}>Search</button>
+            {(typeof weather.lat != 'undefined' && typeof weather.lon!='undefined')?(
             <div>
                 <h2>Weather Info</h2>
+                <div>Your location: {address}</div>
                 {weather.current.temp}
-            </div>
-        ):('')}
+            </div>):('')}
         </div>
     );
 }
