8a4ade83e00e71ff3ad402b19701508240d600a1
Hong Nguyen <s3757107@rmit.edu.vn>
2021-04-15 13:07:55 +0700

    move search bar to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,92 @@
 import Hourly from './component/Hourly';
 import Daily from './component/Daily';
-import React from 'react'
+import React, {useState} from 'react'
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 import Navbar from './component/Navbar'
 import Current from "./component/Current";
 import Footer from './component/Footer'
 import './index.css'
+import PlacesAutocomplete, {geocodeByAddress,getLatLng} from "react-places-autocomplete";
+
+const api = {
+  key: "2bf14f2db250719b59f4c8cc5eb9eb9c",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+  const [message, setMessage] = useState('This is message from App.jsx')
+  const [message1, setMessage1] = useState('This is another message from App.jsx')
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lng: null
+  });
+  const [weather, setWeather] = useState({});
+  const [location, setLocation] = useState("");
+  const [address, setAddress] = useState("Ho Chi Minh City, Vietnam");
+  //async/await function to get the data from Google Maps API
+  const handleSelect = async value => {
+    const results = await geocodeByAddress(value); //Get geocode by address
+    const latLng = await getLatLng(results[0]); //Get latitude and longtitude values
+    setLocation(value); //Set the location to the value
+    setAddress(value);
+    setCoordinates(latLng); //Set latitude and longtitude
+  };
+  const search = evt =>{
+    //Declare the url as a constant value
+    const url = `${api.base}onecall?lat=${coordinates.lat}&lon=${coordinates.lng}&exclude=minutely&units=metric&appid=${api.key}`
+    //Fetching the API
+    fetch(url)
+    .then(res=> res.json()) //Format the result to JSON
+    .then(json => {
+        setWeather(json); //Set data of JSON file to weather
+        console.log(json);
+        setLocation(""); //Set location to null
+        setCoordinates({
+          lat:null,
+          lng: null
+        })
+    });
+    //Test the URL
+    console.log(url) 
+    }
     return (
         <div className = "web">
+          <br/>
           <Router>
+          <div className="input-group mb-3 justify-content-center">
+                <PlacesAutocomplete value={location} onChange={setLocation} onSelect={handleSelect}>
+                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                        <div>
+                            <div>
+                                <input style={{width:'1000px'}} {...getInputProps({ placeholder: "Search for location..." })} className="form-control"/> 
+                            </div>
+                            <div className = "autocomplete-dropdown-container">
+                                {loading ? <div>...loading</div> : null} 
+                                {suggestions.map(suggestion => {
+                                const style = { backgroundColor: suggestion.active ? "#41b6e6" : "#fff" };
+                                return (
+                                    <div >
+                                        <div {...getSuggestionItemProps(suggestion, { style })} className="recommendations" style={{width: '1000px', backgroundColor: 'white'}}>
+                                            {suggestion.description}
+                                        </div> 
+                                    </div>);
+                                })}
+                            </div>
+                        </div>)}
+                    </PlacesAutocomplete>
+                <div>
+                     
+                </div>
+                <div className="input-group-append">
+                    <button className="btn btn-success" type="button" id="button-addon2" onClick = {search}>
+                        <i className="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
             <Navbar/>
             <br/>
             <Switch>
-              <Route exact path="/" component={Current} />
+              <Route exact path="/"> <Current weather = {weather} address = {address}/> </Route>
               <Route exact path="/hourly" component={Hourly} />
               <Route exact path="/daily" component={Daily} />
             </Switch>
--- src/component/Current.jsx
+++ src/component/Current.jsx
@@ -3,72 +3,12 @@ import React, {useState} from 'react'
 import PlacesAutocomplete, {geocodeByAddress,getLatLng} from "react-places-autocomplete";
 import {Button, Modal, Card, Row, Col} from 'react-bootstrap'
 
-//API set up
-const api = {
-    key: "2bf14f2db250719b59f4c8cc5eb9eb9c",
-    base: "https://api.openweathermap.org/data/2.5/"
-}
 
-function Current() {
-    //Create hook for weather and setWeather function
-    const [weather, setWeather] = useState({});
-    //Create hook for location and setLocation function
-    const [location, setLocation] = useState("");
-    //Create hook for coordinates which combines 2 elements (lat and lng values) and setCoordinates function
-    const [coordinates, setCoordinates] = useState({
-        lat: null,
-        lng: null
-    });
-    const [address, setAddress] = useState("Ho Chi Minh City, Vietnam");
+const Current = ({weather, address}) => {
+    
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    //Search event (fetching the weather data from API)
-    const search = evt =>{
-        //Declare the url as a constant value
-        const url = `${api.base}onecall?lat=${coordinates.lat}&lon=${coordinates.lng}&exclude=minutely&units=metric&appid=${api.key}`
-        //Fetching the API
-        fetch(url)
-        .then(res=> res.json()) //Format the result to JSON
-        .then(json => {
-            setWeather(json); //Set data of JSON file to weather
-            console.log(json);
-            setLocation(""); //Set location to null
-            setCoordinates({
-              lat:null,
-              lng: null
-            })
-        });
-        //Test the URL
-        console.log(url) 
-    }
-
-    const fetchData = () =>{
-        fetch(`${api.base}onecall?lat=10.8231&lon=106.6297&exclude=daily,minutely,hourly&units=metric&appid=${api.key}`)
-            .then(res => res.json())
-            .then(json => {
-                setWeather(json); //Set data of JSON file to weather
-                console.log(json);
-                setLocation(""); //Set location to null
-                setCoordinates({
-                  lat: null,
-                  lng: null
-                })
-            });
-    }
-
-    const componentDidMount = () =>  {
-        fetchData();
-    }
-    //async/await function to get the data from Google Maps API
-    const handleSelect = async value => {
-        const results = await geocodeByAddress(value); //Get geocode by address
-        const latLng = await getLatLng(results[0]); //Get latitude and longtitude values
-        setLocation(value); //Set the location to the value
-        setAddress(value);
-        setCoordinates(latLng); //Set latitude and longtitude
-    };
-
     // Convert unix to time
     const convert = (unix) => {
         const date = new Date((unix) * 1000);
@@ -80,36 +20,6 @@ function Current() {
 
     return (
         <div>
-            <div className="input-group mb-3 justify-content-center">
-                <PlacesAutocomplete value={location} onChange={setLocation} onSelect={handleSelect}>
-                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-                        <div>
-                            <div>
-                                <input style={{width:'1000px'}} {...getInputProps({ placeholder: "Search for location..." })} className="form-control"/> 
-                            </div>
-                            <div className = "autocomplete-dropdown-container">
-                                {loading ? <div>...loading</div> : null} 
-                                {suggestions.map(suggestion => {
-                                const style = { backgroundColor: suggestion.active ? "#41b6e6" : "#fff" };
-                                return (
-                                    <div >
-                                        <div {...getSuggestionItemProps(suggestion, { style })} className="recommendations" style={{width: '1000px', backgroundColor: 'white'}}>
-                                            {suggestion.description}
-                                        </div> 
-                                    </div>);
-                                })}
-                            </div>
-                        </div>)}
-                    </PlacesAutocomplete>
-                <div>
-                     
-                </div>
-                <div className="input-group-append">
-                    <button className="btn btn-success" type="button" id="button-addon2" onClick = {search}>
-                        <i className="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
             {(typeof weather.lat != 'undefined' && typeof weather.lon!='undefined')?(
             <div className="web-container">
                 {/* Weather alerts */}
@@ -144,10 +54,8 @@ function Current() {
                                 <Button variant="danger" onClick={handleClose}>Close</Button>
                             </Modal.Footer>
                         </Modal>
-                            </Card.Body>
-                            
-                        </Card>
-                        
+                            </Card.Body>    
+                        </Card>   
                     </div>
                 )}
                 <br/>
@@ -210,7 +118,7 @@ function Current() {
                         </Row>
                     </div>
                 </Row>
-            </div>):(<div>{componentDidMount()}</div>)}
+            </div>):('')}
         </div>
     );
 }
