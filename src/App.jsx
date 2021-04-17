cab6e18f9a068ad18531630f6ff668b1e634af6e
quocthang276 <pyn276@gmail.com>
2021-04-17 21:01:20 +0700

    hourly data displayes with time picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,134 @@
 import Hourly from './component/Hourly';
 import Daily from './component/Daily';
-
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navbar from './component/Navbar'
 import Current from "./component/Current";
+// import Footer from './component/Footer'
 import './index.css'
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 
-export default class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }  
-  
-  componentDidMount() {
-    if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(function(position) {
-        console.log("Latitude is :", position.coords.latitude);
-        console.log("Longitude is :", position.coords.longitude);
-      });
-    }
-  }
-  
-  render(){  
-      return(
-        <div className = "web">
-          <Router>
-            <Navbar/>
-            <br/>
-            <Switch>
-              <Route exact path="/" component={Current} />
-              <Route exact path="/hourly" component={Hourly} />
-              <Route exact path="/daily" component={Daily} />
-            </Switch>
-          </Router>
-        
-        </div>
-        
-      )
-  }
+
+const api = {
+  key: "2bf14f2db250719b59f4c8cc5eb9eb9c",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
+function App() {
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lng: null
+  });
+  const [weather, setWeather] = useState({});
+  const [location, setLocation] = useState("");
+  const [address, setAddress] = useState("Ho Chi Minh City, Vietnam");
+  //async/await function to get the data from Google Maps API
+  const handleSelect = async value => {
+    const results = await geocodeByAddress(value); //Get geocode by address
+    const latLng = await getLatLng(results[0]); //Get latitude and longtitude values
+    setLocation(value); //Set the location to the value
+    setCoordinates(latLng); //Set latitude and longtitude
+  };
+  const fetchData = () => {
+    fetch(`${api.base}onecall?lat=10.8231&lon=106.6297&exclude=minutely&units=metric&appid=${api.key}`)
+      .then(res => res.json())
+      .then(json => {
+        setWeather(json); //Set data of JSON file to weather
+
+      });
+  }
+
+  const componentDidMount = () => {
+    fetchData();
+  }
+  const search = evt => {
+    if(location.trim().length === 0){
+      return;
+    }
+    //Declare the url as a constant value
+    const url = `${api.base}onecall?lat=${coordinates.lat}&lon=${coordinates.lng}&exclude=minutely&units=metric&appid=${api.key}`
+    //Fetching the API
+    fetch(url)
+      .then(res => res.json()) //Format the result to JSON
+      .then(json => {
+        setWeather(json); //Set data of JSON file to weather
+        console.log(json);
+        setLocation(""); //Set location to null
+        setAddress(location)
+      });
+    //Test the URL
+    console.log(url)
+  }
+  return (
+    <div className="container-fluid" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
+
+      {(typeof weather.lat != 'undefined' && typeof weather.lon != 'undefined') ? (
+        <div style={{ backgroundColor: "#3aa7dd" }}>
+          <Router>
+            <div className="container-fluid">
+              <div className="row" >
+                <div className="col-1" >
+                  <br />
+                    {/* Navbrand */}
+                    <Link to='/' className="navbar-brand" style={{ marginLeft: '20px', fontFamily: 'Bad Script', fontWeight: 'bold', color: 'white', fontSize: '25px' }}>
+                      WOR
+                    </Link>
+                </div>
+                {/* Search bar */}
+                <div className="col-11"><br />
+                  <div className="input-group mb-3 justify-content-center">
+                    <PlacesAutocomplete value={location} onChange={setLocation} onSelect={handleSelect}>
+                      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                        <div>
+                          <div>
+                            <input
+                              style={{ width: '1000px' }}
+                              {...getInputProps({ placeholder: "Search for location..." })}
+                              className="form-control"
+                            />
+                          </div>
+                          <div className="autocomplete-dropdown-container">
+                            {loading ? <div>...loading</div> : null}
+                            {suggestions.map(suggestion => {
+                              const style = { backgroundColor: suggestion.active ? "#41b6e6" : "#fff" };
+                              return (
+                                <div >
+                                  <div {...getSuggestionItemProps(suggestion, { style })} className="recommendations" style={{ width: '1000px', backgroundColor: 'white' }}>
+                                    {suggestion.description}
+                                  </div>
+                                </div>);
+                            })}
+                          </div>
+                        </div>)}
+                    </PlacesAutocomplete>
+                    <div className="input-group-append">
+                      <button className="btn btn-success" type="button" id="button-addon2" onClick={search}>
+                        <i className="fa fa-search"></i>
+                      </button>
+                    </div>
+                  </div>
+                 </div>
+              </div>
+            </div>
+            
+            {/* web page */}
+            <div className="web container-fluid" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
+              <Navbar />
+              <br />
+              <Switch>
+                <Route exact path="/"> <Current weather={weather} address={address} /> </Route>
+                <Route exact path="/hourly"><Hourly weather={weather} address={address}></Hourly></Route>
+                <Route exact path="/daily"><Daily weather={weather} address={address}></Daily></Route>
+              </Switch>
+              {/* <Footer /> */}
+            </div>
+          </Router>
+        </div>
+      ) : (<div>{componentDidMount()}</div>)}
+
+
+    </div>
+  )
+}
+
+export default App;
--- src/component/Hourly.jsx
+++ src/component/Hourly.jsx
@@ -1,17 +1,20 @@
 //Import libraries
 import React, {useState} from 'react'
-import PlacesAutocomplete, {geocodeByAddress,getLatLng} from "react-places-autocomplete"; 
 import {Button, Modal, Card, Row, Col} from 'react-bootstrap'
 import { format } from "date-fns";
 import TimeRangePicker from '@wojtekmaj/react-timerange-picker'; 
 import { endOfToday, set } from 'date-fns' 
 import TimeRange from 'react-timeline-range-slider'  
 //API set up
-const api = {
-    key: "2bf14f2db250719b59f4c8cc5eb9eb9c",
-    base: "https://api.openweathermap.org/data/2.5/" 
-}
-const now = new Date()
+// const api = {
+//     key: "2bf14f2db250719b59f4c8cc5eb9eb9c",
+//     base: "https://api.openweathermap.org/data/2.5/" 
+// }
+
+
+
+function Hourly( {weather, address, hourly}) {
+    const now = new Date()
 const getTodayAtSpecificHour = (hour = 0) =>
 	set(now, { hours: hour, minutes: 0, seconds: 0, milliseconds: 0 })
 
@@ -21,79 +24,26 @@ const selectedEnd =  getTodayAtSpecificHour(1)
 const startTime =  getTodayAtSpecificHour()
 const endTime =  endOfToday()
 
-function Hourly() {
     const error =() =>(false)
     const selectedInterval = [selectedStart,selectedEnd] 
     const errorHandler = () => ({ error })  
     const onChangeCallback = (selectedInterval) => ({selectedInterval} )
-    const [value, onChange] = useState(['00:00', '10:00']);
 
-    
-      //Create hook for weather and setWeather function
-    const [weather, setWeather] = useState({});
-    //Create hook for location and setLocation function
-    const [location, setLocation] = useState("");
-    //Create hook for coordinates which combines 2 elements (lat and lng values) and setCoordinates function
-    const [coordinates, setCoordinates] = 
-    
-    useState({
-        lat: null,
-        lng: null
-    });
-    const [address, setAddress] = useState("");
+    const [value, onChange] = useState(['00:00', '12:00']);
+
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    const fetchData = () =>{
-        fetch(`${api.base}onecall?lat=21.028511&lon=105.804817&exclude=minutely&units=metric&appid=${api.key}`)
-            .then(res => res.json())
-            .then(json => {
-                setWeather(json); //Set data of JSON file to weather
-                console.log(json);
-                setLocation(""); //Set location to null
-                setCoordinates({
-                  lat: null,
-                  lng: null
-                })
-            });
-    }
-
-    const componentDidMount = () =>  {
-        fetchData();
-    }
-    //Search event (fetching the weather data from API)
-    const search = evt =>{
-        //Declare the url as a constant value
-        const url = `${api.base}onecall?lat=${coordinates.lat}&lon=${coordinates.lng}&exclude=minutely&units=metric&appid=${api.key}`
-        //Fetching the API
-        fetch(url)
-        .then(res=> res.json()) //Format the result to JSON
-        .then(json => {
-            setWeather(json); //Set data of JSON file to weather
-            console.log(json);
-            setLocation(""); //Set location to null
-            setCoordinates({
-              lat: null,
-              lng: null
-            })
-        });
-        //Test the URL
-        console.log(url) 
-    }
-    
-
-    //async/await function to get the data from Google Maps API
-    const handleSelect = async value => {
-        const results = await geocodeByAddress(value); //Get geocode by address
-        const latLng = await getLatLng(results[0]); //Get latitude and longtitude values
-        setLocation(value); //Set the location to the value
-        setAddress(value);
-        setCoordinates(latLng); //Set latitude and longtitude
-    };
+    // const convert = (unix) => {
+    //     const date = new Date(unix * 1000);
+    //     const hour = date.getUTCHours();
+    //     const time = date.toUTCString()
+    //     return time;
+    // }
     const convert = (unix) => {
         const date = new Date(unix * 1000);
-        const hour =date.getUTCHours();
-        const time = date.toUTCString()
+        const utc_time = date.toUTCString()
+        const time = utc_time.slice(-25, -7)
         return time;
     }
     
@@ -104,65 +54,59 @@ function Hourly() {
         const formattedDate = hour + " : " + minute;
         return formattedDate;
     }
+    const convertMMdd = (unix) => {
+        const date = new Date(unix * 1000);
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        const month = months[date.getMonth()];
+        const day = date.getDate();
+        const formattedDate = month + " " + day;
+        return formattedDate;
+    }
 
     return (
         <div>
-            <div className="input-group mb-3 justify-content-center">
-                <PlacesAutocomplete value={location} onChange={setLocation}  onSelect={handleSelect}>
-                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-                        <div>
-                            <div>
-                                <input style={{width:'1000px'}} {...getInputProps({ placeholder: "Search for location..." })} className="form-control"/> 
-                            </div>
-                            <div className = "autocomplete-dropdown-container">
-                                {loading ? <div>...loading</div> : null} 
-                                {suggestions.map(suggestion => {
-                                const style = { backgroundColor: suggestion.active ? "#41b6e6" : "#fff"};
-                                return (
-                                    <div {...getSuggestionItemProps(suggestion, { style })} className = "recommdations" style={{width: '1000px', backgroundColor: 'white'}}>
-                                        {suggestion.description}
-                                    </div>);
-                                })}
-                            </div>
-                        </div>)}
-                    </PlacesAutocomplete>
-               
-                <div className="input-group-append">
-                    <button className="btn btn-success" type="button" id="button-addon2" onClick = {search}>
-                        <i className="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
-            
             <h1>This is hourly page</h1>
-            
-            {(typeof weather.lat != 'undefined' && typeof weather.lon!='undefined')?(
+            {address}
+    
             <div>
-                {!weather.alerts? (''):(
+            {!weather.alerts ? ('') : (
                     <div className="container">
-                    <Card className="text-center" style={{color: "white",width:"400px", marginLeft:"auto", marginRight: "auto", backgroundColor:"#cf615f"}}>
-                        <Card.Body>
-                            <Card.Title>Weather Alert</Card.Title>
-                            <Card.Text>{weather.alerts.map(alert=>(
-                                <div>{alert.event}</div>
-                            ))}</Card.Text>
-                           <Button variant="primary" onClick={handleShow}>Read More</Button>
-                    <Modal show={show} onHide={handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>National Alerts</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            Location: {address} <br/>
-                            Alert: {weather.alerts.map(alert=>(
-                                <li>
-                                <div>
-                                    Sender Name: {alert.sender_name} <br/>
-                                    Start: {alert.start} <br/>
-                                    End: {alert.end} <br/>
-                                    Description: {alert.description} <br/>
-                                </div>
-                                </li>
-                            ))}
+                        <Card className="text-center" style={{ color: "white", width: "400px", marginLeft: "auto", marginRight: "auto", backgroundColor: "#cf615f" }}>
+                            <Card.Body>
+                                <Card.Title>Weather Alert</Card.Title>
+                                <Card.Text>{weather.alerts.map(alert => (
+                                    <div>{alert.event}</div>
+                                ))}</Card.Text>
+                                <Button variant="primary" onClick={handleShow}>Read More</Button>
+                                <Modal show={show} onHide={handleClose}>
+                                    <Modal.Header closeButton>
+                                        <Modal.Title>National Alerts</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        {weather.alerts.map((a, index) => (
+                                            <div key={index}>
+                                                <div role="tabpanel">
+                                                    <ul className="nav nav-tabs" role="tablist">
+                                                        <li role="presentation" className="active"><a href={a.event} aria-controls="uploadTab" role="tab" data-toggle="tab">{a.event}</a>
+                                                        </li>
+                                                    </ul>
+                                                    <div className="tab-content">
+                                                        <div role="tabpanel" className="tab-pane active" id={a.event}>
+                                                            Location: {address} <br />
+                                                            Alert: {weather.alerts.map(alert => (
+
+                                                            <div>
+                                                                Sender Name: {alert.sender_name} <br />
+                                                                    Start: {convert(alert.start)} <br />
+                                                                    End: {convert(alert.end)} <br />
+                                                                    Description: {alert.description} <br />
+                                                            </div>
+                                                        ))}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
                         </Modal.Body>
                         <Modal.Footer>
                             <Button variant="danger" onClick={handleClose}>Close</Button>
@@ -175,12 +119,12 @@ function Hourly() {
                 </div>)}
                 <Row>
                 <div className="container">
-                    <div className="info">
+                    {/* <div className="info">
                         <span>Selected Interval: </span>
                         {selectedInterval.map((d, i) => (
                             <span key={i}>{format(d, "dd MMM, HH:mm")}</span>
                         ))}
-                    </div>
+                    </div> */}
                     <TimeRange
                         ticksNumber={36}  
                         selectedInterval = {selectedInterval}
@@ -191,27 +135,6 @@ function Hourly() {
                 </div>
                 <div>
                     {console.log(weather)}
-
-                    <div>
-                        <div>
-                            <TimeRangePicker
-                                onChange={onChange}
-                                value= {value}
-                                disableClock= 'true'
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    {weather.hourly.filter(data => (value[0] <= converthhmm(data.dt) && converthhmm(data.dt) <= value[1])).map((data, index) => (
-                        <div key={index}>
-                            {converthhmm(data.dt)}
-                            {data.weather.map(weather=>(
-                                weather.main
-                            )
-                            )}
-                        </div>
-                     ))}
                 </div>
 
                 </Row>
@@ -232,26 +155,38 @@ function Hourly() {
                         <h2>Details</h2>
                         <br/>
                         <Row>
-                                <hr/>
-                                <Row>{weather.hourly.map(hourly=>(
-                                    <div>
+                            <div>
+                                    <TimeRangePicker
+                                onChange={onChange}
+                                value= {value}
+                                disableClock= 'true'
+                                />
+                               
+                            </div>
+                        </Row>
+                        <Row>
+                            <hr/>
+                                <Row>
+                                    {weather.hourly.filter(data => (value[0] <= converthhmm(data.dt) && converthhmm(data.dt) <= value[1])).map((data, index) => (
+                                    <div key={index}>
                                         <Row>
-                                            <Col><p>{convert(hourly.dt)}</p></Col>
-                                            <Col><p>Temperatures: {hourly.temp.toFixed(0)}°C</p></Col>              
-                                            <Col><p>Humidity: {hourly.humidity} %</p></Col>                                                                                                                                                           
-                                            <Col><p> Wind speed: {hourly.wind_speed.toFixed(0) * 3.6} km/h</p></Col>   
-                                            <Col><p> UV index: {hourly.uvi}</p></Col>
+                                            <Col><p>{convert(data.dt * 1 + weather.timezone_offset * 1)}</p></Col>
+                                            <Col><p>Temperatures: {data.temp.toFixed(0)}°C</p></Col>              
+                                            <Col><p>Humidity: {data.humidity} %</p></Col>                                                                                                                                                           
+                                            <Col><p> Wind speed: {data.wind_speed.toFixed(0) * 3.6} km/h</p></Col>   
+                                            <Col><p> UV index: {data.uvi}</p></Col>
                                             {/* <Col><p>{hourly.weather}</p></Col> */}
                                             <hr/>
                                         </Row>
                                     </div>
-                                ))}</Row>
+                                ))}
+                                </Row>
                         </Row>
                     </div>
                 </Row>
-                <div>Your location: {weather.address}</div>
-                {weather.current.temp}
-            </div>):(<div>{componentDidMount()}</div>)}
+                
+            </div>        
+
             
         </div>
     );
