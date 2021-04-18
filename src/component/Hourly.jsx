6686cccd3b58764783f7c2776acb15d496bda09c
Hong Nguyen <s3757107@rmit.edu.vn>
2021-04-18 10:57:35 +0700

    Tomorrow tab implemented

--- src/component/Hourly.jsx
+++ src/component/Hourly.jsx
@@ -1,62 +1,54 @@
 //Import libraries
-import React, {useState} from 'react'
-import {Button, Modal, Card, Row, Col} from 'react-bootstrap'
-import TimeRangePicker from '@wojtekmaj/react-timerange-picker'; 
+import React, { useState } from 'react'
+import { Button, Modal, Card, Row, Col, Table, Tabs, Tab } from 'react-bootstrap'
+import TimeRangePicker from '@wojtekmaj/react-timerange-picker';
 
 
 
 
-
-function Hourly({weather, address}) {
-
-
-    const [value, onChange] = useState(['00:00','23:00']);
+function Hourly({ weather, address }) {
+    const [value, onChange] = useState(['01:00', '23:00']);
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    
+
     const convert = (unix) => {
         const date = new Date(unix * 1000);
         const utc_time = date.toUTCString()
-        const time = utc_time.slice(-12,-7)
+        const time = utc_time.slice(-12, -7)
         return time;
     }
     const convertDate = (unix) => {
         const date = new Date(unix * 1000);
         const utc_time = date.toUTCString()
-        const time = utc_time.slice(-25,-7)
+        const time = utc_time.slice(-25, -7)
         return time;
     }
-
-    const compare = (time) => {
+    const todaycompare = (time) => {
         var today = new Date()
         const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-        let datetime = date + " " +time
-        let unix  = (Date.parse(datetime)/1000).toFixed(0)
-        {console.log(`${date}${time}`)}
-        {console.log(datetime)}
-        {console.log("Unix: " + {unix})}
+        let datetime = date + " " + time
+        let unix = (Date.parse(datetime) / 1000).toFixed(0)
         return unix;
     }
-    
-    // const converthhmm = (unix) => {
-    //     const date = new Date(unix * 1000);
-    //     const hour = date.getUTCHours();
-    //     const minute = date.getUTCMinutes();
-    //     const formattedDate = (hour + " : " + minute);
-    //     return formattedDate;
-    // }
-   
 
-    
+    const tmrcompare = (time) => {
+        var today = new Date()
+        const tomorrow = new Date(today)
+        tomorrow.setDate(tomorrow.getDate() + 1)
+        const date = tomorrow.getFullYear() + '-' + (tomorrow.getMonth() + 1) + '-' + tomorrow.getDate();
+        let datetime = date + " " + time
+        let unix = (Date.parse(datetime) / 1000).toFixed(0)
+        return unix;
+    }
+
+    const [key, setKey] = useState('today');
+
     return (
         <div>
-            <h1>This is hourly page</h1>
-            {address}
-    
             <div>
-            {!weather.alerts ? ('') : (
+                {!weather.alerts ? ('') : (
                     <div className="container">
                         <Card className="text-center" style={{ color: "white", width: "400px", marginLeft: "auto", marginRight: "auto", backgroundColor: "#cf615f" }}>
                             <Card.Body>
@@ -81,7 +73,6 @@ function Hourly({weather, address}) {
                                                         <div role="tabpanel" className="tab-pane active" id={a.event}>
                                                             Location: {address} <br />
                                                             Alert: {weather.alerts.map(alert => (
-
                                                             <div>
                                                                 Sender Name: {alert.sender_name} <br />
                                                                     Start: {convert(alert.start)} <br />
@@ -94,65 +85,147 @@ function Hourly({weather, address}) {
                                                 </div>
                                             </div>
                                         ))}
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="danger" onClick={handleClose}>Close</Button>
-                        </Modal.Footer>
-                    </Modal>
-                        </Card.Body>
-                        
-                    </Card>
-                    
-                </div>)}
-                <h2>Weather Info</h2>
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button variant="danger" onClick={handleClose}>Close</Button>
+                                    </Modal.Footer>
+                                </Modal>
+                            </Card.Body>
+                        </Card>
+                    </div>)}
 
                 <Row>
-                    
 
-                    <div className = "col">
-                        <div className = "outfit-box">
+
+                    <div className="col">
+                        <div className="outfit-box">
                             <h2>Recommendation</h2>
                         </div>
                     </div>
                 </Row>
-                
+
                 <Row>
-                    <div className = "detail-box">
+                    <div className="detail-box">
                         <h2>Details</h2>
-                        <br/>
+                        <br />
+
+                        <hr />
                         <Row>
-                            <div>
+                            <Col><b>Time selection</b></Col>
+                            <Col><div>
                                 <TimeRangePicker
-                                onChange={onChange}
-                                value= {value}
-                                disableClock= 'true'
+                                    onChange={onChange}
+                                    value={value}
+                                    disableClock='true'
                                 />
-                            </div>
+
+                            </div></Col>
                         </Row>
                         <Row>
-                            <hr/>
-                                <Row>
-                                  {weather.hourly.filter(data => (compare(value[0]) <= data.dt && data.dt <= compare(value[1]))).map((data, index)=> (
-                                    <div key={index}
-                                    >
-                                        <Row>                                   
-                                            <Col><p>{convertDate(data.dt + weather.timezone_offset *1)}</p></Col>
-                                            <Col><p>Temperatures: {data.temp.toFixed(0)}°C</p></Col>              
-                                            <Col><p>Humidity: {data.humidity} %</p></Col>                                                                                                                                                           
-                                            <Col><p> Wind speed: {data.wind_speed.toFixed(0) * 3.6} km/h</p></Col>   
-                                            <Col><p> UV index: {data.uvi}</p></Col>                                                       
-                                            <hr/>
+                            <br />
+                            <Tabs
+                                id="controlled-tab-example"
+                                activeKey={key}
+                                onSelect={(k) => setKey(k)}
+                            >
+                                <Tab eventKey="today" title="Today">
+                                    <Row>
+
+
+                                        <div >
+                                            <Table>
+                                                <thead>
+                                                    <tr>
+                                                        <th><b>Time</b></th>
+                                                        <th><b>Temperatures</b></th>
+                                                        <th><b>Humidity</b></th>
+                                                        <th><b>Rain probability</b></th>
+                                                        <th><b>UV Index</b></th>
+                                                    </tr>
+                                                </thead>
+                                                {weather.hourly.filter(data => (todaycompare(value[0]) <= data.dt && data.dt <= todaycompare(value[1]))).map((data, index) => (
+                                                    <tbody key={index}>
+                                                        <tr data-toggle="collapse"
+                                                            data-target=".multi-collapse1"
+                                                            aria-controls="multiCollapseExample1">
+
+                                                            <td>{convertDate(data.dt + weather.timezone_offset * 1)}</td>
+                                                            <td>{data.temp.toFixed(0)}°C</td>
+                                                            <td>{data.humidity} %</td>
+                                                            <td>{data.pop} % </td>
+                                                            <td>{data.uvi}</td>
+                                                        </tr>
+                                                        <tr class="collapse multi-collapse1" id="multiCollapseExample1"> <b>
+                                                            <td>Wind speed: {data.wind_speed.toFixed(0) * 3.6} km/h </td>
+                                                            <td>Pressure:  {data.pressure} hPa</td>
+                                                            <td>Dew point: {data.dew_point.toFixed(0)}°C</td>
+                                                            <td>Visibility: {data.visibility / 1000} km</td>
+                                                            <td>Cloudiness: {data.clouds} % </td>
+                                                            <td>Wind gust: {data.wind_gust.toFixed(0) * 3.6} km/h</td>
+                                                        </b>
+                                                        </tr>
+                                                    </tbody>
+                                                ))}
+                                            </Table>
+                                        </div>
+                                    </Row>
+                                </Tab>
+                                <Tab eventKey="tomorrow" title="Tomorrow">
+                                    <div>
+                                        <Row>
+                                            <div >
+                                                <Table>
+                                                    <thead>
+                                                        <tr>
+                                                            <th><b>Time</b></th>
+                                                            <th><b>Temperatures</b></th>
+                                                            <th><b>Humidity</b></th>
+                                                            <th><b>Rain probability</b></th>
+                                                            <th><b>UV Index</b></th>
+                                                        </tr>
+                                                    </thead>
+                                                    {weather.hourly.filter(data => (tmrcompare(value[0]) <= data.dt && data.dt <= tmrcompare(value[1]))).map((data, index) => (
+                                                        <tbody key={index}>
+                                                            <tr data-toggle="collapse"
+                                                                data-target=".multi-collapse1"
+                                                                aria-controls="multiCollapseExample1">
+
+                                                                <td>{convertDate(data.dt + weather.timezone_offset * 1)}</td>
+                                                                <td>{data.temp.toFixed(0)}°C</td>
+                                                                <td>{data.humidity} %</td>
+                                                                <td>{data.pop} % </td>
+                                                                <td>{data.uvi}</td>
+
+                                                            </tr>
+
+                                                            <tr class="collapse multi-collapse1" id="multiCollapseExample1"> <b>
+
+                                                                <td>Wind speed: {data.wind_speed.toFixed(0) * 3.6} km/h </td>
+                                                                <td>Pressure:  {data.pressure} hPa</td>
+                                                                <td>Dew point: {data.dew_point.toFixed(0)}°C</td>
+                                                                <td>Visibility: {data.visibility / 1000} km</td>
+                                                                <td>Cloudiness: {data.clouds} % </td>
+                                                                <td>Wind gust: {data.wind_gust.toFixed(0) * 3.6} km/h</td>
+                                                            </b>
+                                                            </tr>
+                                                        </tbody>
+                                                    ))}
+                                                </Table>
+                                            </div>
                                         </Row>
                                     </div>
-                                ))}
-                                </Row>
+                                </Tab>
+
+                            </Tabs>
+                            <hr />
+
                         </Row>
                     </div>
                 </Row>
-                
-            </div>        
 
-            
+            </div>
+
+
         </div>
     );
 }
