64f694653d96a6263405ba295d53393e83ff683d
quocthang276 <pyn276@gmail.com>
2021-04-18 08:02:22 +0700

    filter function worked

--- src/component/Hourly.jsx
+++ src/component/Hourly.jsx
@@ -1,68 +1,43 @@
 //Import libraries
 import React, {useState} from 'react'
 import {Button, Modal, Card, Row, Col} from 'react-bootstrap'
-import { format } from "date-fns";
 import TimeRangePicker from '@wojtekmaj/react-timerange-picker'; 
-import { endOfToday, set } from 'date-fns' 
-import TimeRange from 'react-timeline-range-slider'  
-//API set up
-// const api = {
-//     key: "2bf14f2db250719b59f4c8cc5eb9eb9c",
-//     base: "https://api.openweathermap.org/data/2.5/" 
-// }
 
 
 
-function Hourly( {weather, address, hourly}) {
-    const now = new Date()
-const getTodayAtSpecificHour = (hour = 0) =>
-	set(now, { hours: hour, minutes: 0, seconds: 0, milliseconds: 0 })
 
-const selectedStart =  getTodayAtSpecificHour(0)
-const selectedEnd =  getTodayAtSpecificHour(1)
 
-const startTime =  getTodayAtSpecificHour()
-const endTime =  endOfToday()
+function Hourly({weather, address}) {
 
-    const error =() =>(false)
-    const selectedInterval = [selectedStart,selectedEnd] 
-    const errorHandler = () => ({ error })  
-    const onChangeCallback = (selectedInterval) => ({selectedInterval} )
-
-    const [value, onChange] = useState(['00:00', '12:00']);
+    const [value, onChange] = useState(['00:00','23:00']);
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    // const convert = (unix) => {
-    //     const date = new Date(unix * 1000);
-    //     const hour = date.getUTCHours();
-    //     const time = date.toUTCString()
-    //     return time;
-    // }
+    
     const convert = (unix) => {
         const date = new Date(unix * 1000);
         const utc_time = date.toUTCString()
-        const time = utc_time.slice(-25, -7)
+        const time = utc_time.slice(-12,-7)
+        return time;
+    }
+    const convertDate = (unix) => {
+        const date = new Date(unix * 1000);
+        const utc_time = date.toUTCString()
+        const time = utc_time.slice(-25,-7)
         return time;
     }
     
-    const converthhmm = (unix) => {
-        const date = new Date(unix * 1000);
-        const hour = date.getUTCHours();
-        const minute = date.getUTCMinutes();
-        const formattedDate = hour + " : " + minute;
-        return formattedDate;
-    }
-    const convertMMdd = (unix) => {
-        const date = new Date(unix * 1000);
-        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        const month = months[date.getMonth()];
-        const day = date.getDate();
-        const formattedDate = month + " " + day;
-        return formattedDate;
-    }
+    // const converthhmm = (unix) => {
+    //     const date = new Date(unix * 1000);
+    //     const hour = date.getUTCHours();
+    //     const minute = date.getUTCMinutes();
+    //     const formattedDate = (hour + " : " + minute);
+    //     return formattedDate;
+    // }
+   
 
+    
     return (
         <div>
             <h1>This is hourly page</h1>
@@ -117,27 +92,6 @@ const endTime =  endOfToday()
                     </Card>
                     
                 </div>)}
-                <Row>
-                <div className="container">
-                    {/* <div className="info">
-                        <span>Selected Interval: </span>
-                        {selectedInterval.map((d, i) => (
-                            <span key={i}>{format(d, "dd MMM, HH:mm")}</span>
-                        ))}
-                    </div> */}
-                    <TimeRange
-                        ticksNumber={36}  
-                        selectedInterval = {selectedInterval}
-                        timelineInterval={[startTime, endTime]}  
-                        onUpdateCallback={errorHandler}  
-                        onChangeCallback={onChangeCallback}  
-                    />
-                </div>
-                <div>
-                    {console.log(weather)}
-                </div>
-
-                </Row>
                 <h2>Weather Info</h2>
 
                 <Row>
@@ -167,15 +121,16 @@ const endTime =  endOfToday()
                         <Row>
                             <hr/>
                                 <Row>
-                                    {weather.hourly.filter(data => (value[0] <= converthhmm(data.dt) && converthhmm(data.dt) <= value[1])).map((data, index) => (
-                                    <div key={index}>
-                                        <Row>
-                                            <Col><p>{convert(data.dt * 1 + weather.timezone_offset * 1)}</p></Col>
+
+                                  {weather.hourly.filter(data => (value[0] <= convert(data.dt + weather.timezone_offset *1) && convert(data.dt + weather.timezone_offset*1) <= value[1])).map((data, index)=> (
+                                    <div key={index}
+                                    >
+                                        <Row>                                   
+                                            <Col><p>{convertDate(data.dt + weather.timezone_offset *1)}</p></Col>
                                             <Col><p>Temperatures: {data.temp.toFixed(0)}°C</p></Col>              
                                             <Col><p>Humidity: {data.humidity} %</p></Col>                                                                                                                                                           
                                             <Col><p> Wind speed: {data.wind_speed.toFixed(0) * 3.6} km/h</p></Col>   
-                                            <Col><p> UV index: {data.uvi}</p></Col>
-                                            {/* <Col><p>{hourly.weather}</p></Col> */}
+                                            <Col><p> UV index: {data.uvi}</p></Col>                                                       
                                             <hr/>
                                         </Row>
                                     </div>
