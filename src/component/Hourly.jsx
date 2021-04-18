626e0954a50c2b1f3973271dd30ab2fb2bac1c4c
quocthang276 <pyn276@gmail.com>
2021-04-18 10:42:26 +0700

    detail informtaion table for hourly

--- src/component/Hourly.jsx
+++ src/component/Hourly.jsx
@@ -1,15 +1,13 @@
 //Import libraries
 import React, {useState} from 'react'
-import {Button, Modal, Card, Row, Col} from 'react-bootstrap'
+import {Button, Modal, Card, Row, Col, Table} from 'react-bootstrap'
 import TimeRangePicker from '@wojtekmaj/react-timerange-picker'; 
-
-
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 
 
 function Hourly({weather, address}) {
-
-    const [value, onChange] = useState(['00:00','23:00']);
+    const [value, onChange] = useState(['01:00','23:00']);
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -27,22 +25,33 @@ function Hourly({weather, address}) {
         const time = utc_time.slice(-25,-7)
         return time;
     }
-    
-    // const converthhmm = (unix) => {
-    //     const date = new Date(unix * 1000);
-    //     const hour = date.getUTCHours();
-    //     const minute = date.getUTCMinutes();
-    //     const formattedDate = (hour + " : " + minute);
-    //     return formattedDate;
-    // }
+    const compare = (time) => {
+        var today = new Date()
+        const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        let datetime = date + " " +time
+        let unix  = (Date.parse(datetime)/1000).toFixed(0)
+        {console.log(`${date}${time}`)}
+        {console.log(datetime)}
+        {console.log("Unix: " + {unix})}
+        return unix;
+    }
+        
+    const converthhmm = (unix) => {
+        const date = new Date(unix * 1000);
+        const hour = date.getUTCHours();
+        const minute = date.getUTCMinutes();
+        const formattedDate = (hour + " : " + minute);
+        return formattedDate;
+    }
    
+
+      
 
     
     return (
         <div>
-            <h1>This is hourly page</h1>
+
             {address}
-    
             <div>
             {!weather.alerts ? ('') : (
                     <div className="container">
@@ -108,33 +117,76 @@ function Hourly({weather, address}) {
                     <div className = "detail-box">
                         <h2>Details</h2>
                         <br/>
+                        
+                        <hr/>
                         <Row>
-                            <div>
-                                    <TimeRangePicker
+                            <Col><b>Time selection</b></Col>
+                           <Col><div>
+                                    <TimeRangePicker 
                                 onChange={onChange}
                                 value= {value}
                                 disableClock= 'true'
                                 />
                                
-                            </div>
+                            </div></Col>
+                            <Col></Col>
+                         
                         </Row>
                         <Row>
                             <hr/>
                                 <Row>
 
-                                  {weather.hourly.filter(data => (value[0] <= convert(data.dt + weather.timezone_offset *1) && convert(data.dt + weather.timezone_offset*1) <= value[1])).map((data, index)=> (
-                                    <div key={index}
-                                    >
-                                        <Row>                                   
-                                            <Col><p>{convertDate(data.dt + weather.timezone_offset *1)}</p></Col>
+                                
+                                    <div >
+                                        <Table> 
+                                            <thead>
+                                                <tr>
+                                                    <th><b>Time</b></th>
+                                                    <th><b>Temperatures</b></th>
+                                                    <th><b>Humidity</b></th>
+                                                    <th><b>Rain probability</b></th>
+                                                    <th><b>UV Index</b></th>
+                                                </tr>
+                                            </thead>
+                                            {weather.hourly.filter(data => (compare(value[0]) <= data.dt && data.dt <= compare(value[1]))).map((data, index)=> (
+                                            <tbody key = {index}>
+                                                <tr  data-toggle="collapse"
+                                                    data-target=".multi-collapse1"
+                                                    aria-controls="multiCollapseExample1">
+                                                                                                                
+                                                        <td>{convertDate(data.dt + weather.timezone_offset *1)}</td>
+                                                        <td>{data.temp.toFixed(0)}°C</td>
+                                                        <td>{data.humidity} %</td>
+                                                        <td>{data.pop} % </td>
+                                                        <td>{data.uvi}</td>
+                                                        
+                                               </tr>
+                                                
+                                                <tr class="collapse multi-collapse1" id="multiCollapseExample1"> <b>
+                                                    
+                                                    <td>Wind speed: {data.wind_speed.toFixed(0) * 3.6} km/h </td>
+                                                    <td>Pressure:  {data.pressure} hPa</td>
+                                                    <td>Dew point: {data.dew_point.toFixed(0)}°C</td>
+                                                    <td>Visibility: {data.visibility/1000} km</td>
+                                                    <td>Cloudiness: {data.clouds} % </td>
+                                                    <td>Wind gust: {data.wind_gust.toFixed(0) *3.6} km/h</td>
+
+                                                    </b>
+                                                </tr>      
+                                                                      
+                                            {/* <Col><p>{convertDate(data.dt + weather.timezone_offset *1)}</p></Col>
                                             <Col><p>Temperatures: {data.temp.toFixed(0)}°C</p></Col>              
                                             <Col><p>Humidity: {data.humidity} %</p></Col>                                                                                                                                                           
                                             <Col><p> Wind speed: {data.wind_speed.toFixed(0) * 3.6} km/h</p></Col>   
-                                            <Col><p> UV index: {data.uvi}</p></Col>                                                       
-                                            <hr/>
-                                        </Row>
+                                            <Col><p> UV index: {data.uvi}</p></Col> */}
+                                                                                                 
+                                            
+                                            </tbody> 
+                                            ))}
+                                            
+                                        </Table>
                                     </div>
-                                ))}
+                                
                                 </Row>
                         </Row>
                     </div>
