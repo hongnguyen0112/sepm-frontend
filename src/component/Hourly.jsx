0f43eba8b3429a601e092b040258f815bd90accf
Hong Nguyen <s3757107@rmit.edu.vn>
2021-04-18 08:35:02 +0700

    date time convert to unix

--- src/component/Hourly.jsx
+++ src/component/Hourly.jsx
@@ -1,68 +1,62 @@
 //Import libraries
-import React, { useState } from 'react'
-import { Button, Modal, Card, Row, Col } from 'react-bootstrap'
-import { format } from "date-fns";
-import TimeRangePicker from '@wojtekmaj/react-timerange-picker';
-import { endOfToday, set } from 'date-fns'
-import TimeRange from 'react-timeline-range-slider'
+import React, {useState} from 'react'
+import {Button, Modal, Card, Row, Col} from 'react-bootstrap'
+import TimeRangePicker from '@wojtekmaj/react-timerange-picker'; 
 
-function Hourly({ weather, address, hourly }) {
-    const now = new Date()
-    const getTodayAtSpecificHour = (hour = 0) =>
-        set(now, { hours: hour, minutes: 0, seconds: 0, milliseconds: 0 })
 
-    const selectedStart = getTodayAtSpecificHour(0)
-    const selectedEnd = getTodayAtSpecificHour(1)
 
-    const startTime = getTodayAtSpecificHour()
-    const endTime = endOfToday()
 
-    const error = () => (false)
-    const selectedInterval = [selectedStart, selectedEnd]
-    const errorHandler = () => ({ error })
-    const onChangeCallback = (selectedInterval) => ({ selectedInterval })
 
-    const [value, onChange] = useState(['00:00', '12:00']);
+function Hourly({weather, address}) {
+
+
+    const [value, onChange] = useState(['00:00','23:00']);
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    // const convert = (unix) => {
-    //     const date = new Date(unix * 1000);
-    //     const hour = date.getUTCHours();
-    //     const time = date.toUTCString()
-    //     return time;
-    // }
+    
     const convert = (unix) => {
         const date = new Date(unix * 1000);
         const utc_time = date.toUTCString()
-        const time = utc_time.slice(-25, -7)
+        const time = utc_time.slice(-12,-7)
+        return time;
+    }
+    const convertDate = (unix) => {
+        const date = new Date(unix * 1000);
+        const utc_time = date.toUTCString()
+        const time = utc_time.slice(-25,-7)
         return time;
     }
 
-    const converthhmm = (unix) => {
-        const date = new Date(unix * 1000);
-        const hour = date.getUTCHours();
-        const minute = date.getUTCMinutes();
-        const formattedDate = hour + " : " + minute;
-        return formattedDate;
+    const compare = (time) => {
+        var today = new Date()
+        const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        let datetime = date + " " +time
+        let unix  = Date.parse(datetime)/1000
+        {console.log(`${date}${time}`)}
+        {console.log(datetime)}
+        {console.log("Unix: " + {unix})}
+        return unix;
     }
-    const convertMMdd = (unix) => {
-        const date = new Date(unix * 1000);
-        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        const month = months[date.getMonth()];
-        const day = date.getDate();
-        const formattedDate = month + " " + day;
-        return formattedDate;
-    }
+    
+    // const converthhmm = (unix) => {
+    //     const date = new Date(unix * 1000);
+    //     const hour = date.getUTCHours();
+    //     const minute = date.getUTCMinutes();
+    //     const formattedDate = (hour + " : " + minute);
+    //     return formattedDate;
+    // }
+   
 
+    
     return (
         <div>
             <h1>This is hourly page</h1>
             {address}
-
+    
             <div>
-                {!weather.alerts ? ('') : (
+            {!weather.alerts ? ('') : (
                     <div className="container">
                         <Card className="text-center" style={{ color: "white", width: "400px", marginLeft: "auto", marginRight: "auto", backgroundColor: "#cf615f" }}>
                             <Card.Body>
@@ -100,87 +94,65 @@ function Hourly({ weather, address, hourly }) {
                                                 </div>
                                             </div>
                                         ))}
-                                    </Modal.Body>
-                                    <Modal.Footer>
-                                        <Button variant="danger" onClick={handleClose}>Close</Button>
-                                    </Modal.Footer>
-                                </Modal>
-                            </Card.Body>
-
-                        </Card>
-
-                    </div>)}
-                <Row>
-                    <div className="container">
-                        {/* <div className="info">
-                        <span>Selected Interval: </span>
-                        {selectedInterval.map((d, i) => (
-                            <span key={i}>{format(d, "dd MMM, HH:mm")}</span>
-                        ))}
-                    </div> */}
-                        <TimeRange
-                            ticksNumber={36}
-                            selectedInterval={selectedInterval}
-                            timelineInterval={[startTime, endTime]}
-                            onUpdateCallback={errorHandler}
-                            onChangeCallback={onChangeCallback}
-                        />
-                    </div>
-                    <div>
-                        {console.log(weather)}
-                    </div>
-
-                </Row>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="danger" onClick={handleClose}>Close</Button>
+                        </Modal.Footer>
+                    </Modal>
+                        </Card.Body>
+                        
+                    </Card>
+                    
+                </div>)}
                 <h2>Weather Info</h2>
-                {console.log(startTime)}
+
                 <Row>
+                    
 
-
-                    <div className="col">
-                        <div className="outfit-box">
+                    <div className = "col">
+                        <div className = "outfit-box">
                             <h2>Recommendation</h2>
                         </div>
                     </div>
                 </Row>
-
+                
                 <Row>
-                    <div className="detail-box">
+                    <div className = "detail-box">
                         <h2>Details</h2>
-                        <br />
+                        <br/>
                         <Row>
                             <div>
                                 <TimeRangePicker
-                                    onChange={onChange}
-                                    value={value}
-                                    disableClock='true'
+                                onChange={onChange}
+                                value= {value}
+                                disableClock= 'true'
                                 />
-
                             </div>
                         </Row>
                         <Row>
-                            <hr />
-                            <Row>
-                                {weather.hourly.filter(hour => (value[0] <= converthhmm(hour.dt) && converthhmm(hour.dt) <= value[1])).map((data, index) => (
-                                    <div key={index}>
-                                        <Row>
-                                            <Col><p>{convert(data.dt * 1 + weather.timezone_offset * 1)}</p></Col>
-                                            <Col><p>Temperatures: {data.temp.toFixed(0)}°C</p></Col>
-                                            <Col><p>Humidity: {data.humidity} %</p></Col>
-                                            <Col><p> Wind speed: {data.wind_speed.toFixed(0) * 3.6} km/h</p></Col>
-                                            <Col><p> UV index: {data.uvi}</p></Col>
-                                            {/* <Col><p>{hourly.weather}</p></Col> */}
-                                            <hr />
+                            <hr/>
+                                <Row>
+                                  {weather.hourly.filter(data => (compare(value[0]) <= data.dt && data.dt <= compare(value[1]))).map((data, index)=> (
+                                    <div key={index}
+                                    >
+                                        <Row>                                   
+                                            <Col><p>{convertDate(data.dt + weather.timezone_offset *1)}</p></Col>
+                                            <Col><p>Temperatures: {data.temp.toFixed(0)}°C</p></Col>              
+                                            <Col><p>Humidity: {data.humidity} %</p></Col>                                                                                                                                                           
+                                            <Col><p> Wind speed: {data.wind_speed.toFixed(0) * 3.6} km/h</p></Col>   
+                                            <Col><p> UV index: {data.uvi}</p></Col>                                                       
+                                            <hr/>
                                         </Row>
                                     </div>
                                 ))}
-                            </Row>
+                                </Row>
                         </Row>
                     </div>
                 </Row>
+                
+            </div>        
 
-            </div>
-
-
+            
         </div>
     );
 }
